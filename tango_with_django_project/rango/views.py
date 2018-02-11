c12ed0ad8e1cae3ae78aeacaff9f620d85aad82e
Kamal Deepak <2286291d@student.gla.ac.uk>
2018-02-11 19:06:16 +0000

    Chapter-7

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -25,3 +27,35 @@ def index(request):
 def about(request):
     context_dict = {'aboutmessage': "This tutorial has been put together by Kamal Deepak"}
     return render(request,'rango/about.html',context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+            return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+        form = PageForm()
+        if request.method == 'POST':
+            form = PageForm(request.POST)
+            if form.is_valid():
+                if category:
+                    page = form.save(commit=False)
+                    page.category = category
+                    page.views = 0
+                    page.save()
+                    return show_category(request, category_name_slug)
+                else:
+                    print(form.errors)
+                    context_dict = {'form':form, 'category': category}
+                    return render(request, 'rango/add_page.html', context_dict)
